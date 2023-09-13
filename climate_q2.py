d8803165b599626209e61324a530de358a88f632
LachlanDietrich <lachlan.dietrich@griffithuni.edu.au>
2023-09-13 14:43:48 +1000

    Q2 completed

--- climate_q2.py
+++ climate_q2.py
@@ -5,16 +5,20 @@ years = []
 co2 = []
 temp = []
 
+usecols = ["Year", "CO2", "Temperature"]
+df = pd.read_csv('climate.csv', index_col='Year', usecols=usecols)     #   load .csv data
+df.head()
+
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+plt.show()
+plt.savefig("co2_temp_2.png")
 
