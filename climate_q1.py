12592d17bac5eaf61fffbd40f756700f9e1da652
LachlanDietrich <lachlan.dietrich@griffithuni.edu.au>
2023-09-13 14:17:34 +1000

    Q1 completed in python file

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,17 @@
+import sqlite3
 import matplotlib.pyplot as plt
         
 years = []
 co2 = []
 temp = []
+
+connection = sqlite3.connect(r'C:\Users\lachl\Desktop\Quiz_week_8\climate.db')
+cursor = connection.cursor()                #   cursor oject required for db access
+cursor.execute("select * from ClimateData") #   fetching all values from ClimateData table
+print("fetchall:")
+result = cursor.fetchall()                  #   query rows returned as list
+for r in result:
+    print(r)                                #   printing list to the commandline
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
